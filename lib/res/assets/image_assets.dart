4a7af5956c7f5238da69cc6a74bc4d2a09dca0e0
Muntasir Hossen <montachirhossen76@gmail.com>
2023-09-19 17:48:45 +0600

    Assets folders added

---
+++ lib/res/assets/image_assets.dart
@@ -0,0 +1,4 @@
+class ImageAssets {
+  static const String splashSceen = 'assets/images/splash_screen.jpg';
+  static const String message = 'assets/images/message.jpg';
+}
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_mvvm/res/assets/image_assets.dart';
+import 'package:getx_mvvm/res/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,8 +14,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Image(image: AssetImage(ImageAssets.splashSceen)),
       appBar: AppBar(
         title: Text('email_hint'.tr),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Utils.toastMessageCenter("Welcome");
+        },
       ),
     );
   }
